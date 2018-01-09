1045ec0aa9918bea036c94f44edb1706a95c7816
Harrison Healey <harrisonmhealey@gmail.com>
2018-01-09 17:40:11 -0500

    Used a consistent tense for system messages

--- app/team.go
+++ app/team.go
@@ -679,7 +679,7 @@ func (a *App) postRemoveFromTeamMessage(user *model.User, channel *model.Channel
 		Type:      model.POST_REMOVE_FROM_TEAM,
 		UserId:    user.Id,
 		Props: model.StringInterface{
-			"removedUsername": user.Username,
+			"username": user.Username,
 		},
 	}
 
